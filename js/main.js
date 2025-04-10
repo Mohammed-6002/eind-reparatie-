0cf9b45f23780554e29ee9168cd34a4e8df2ad07
Mohammed-6002 <302352550@student.rocmondriaan.nl>
2025-04-10 13:16:53 +0200

    save

--- js/main.js
+++ js/main.js
@@ -389,7 +389,7 @@ function toggleTheme() {
   document.body.classList.toggle('dark-mode');
   const isDarkMode = document.body.classList.contains('dark-mode');
   localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-  elements.themeToggle.innerHTML = isDarkMode ? '&#x1F31E;' : '&#x1F319;'; 
+  elements.themeToggle.innerHTML = isDarkMode ? '&#x1F319;':'&#x1F31E;' ; 
 }
 
 // Initialize theme
@@ -397,9 +397,9 @@ function initTheme() {
   const savedTheme = localStorage.getItem('theme') || 'light';
   if (savedTheme === 'dark') {
     document.body.classList.add('dark-mode');
-    elements.themeToggle.innerHTML = '&#x1F31E;'; 
-  } else {
     elements.themeToggle.innerHTML = '&#x1F319;'; 
+  } else {
+    elements.themeToggle.innerHTML = '&#x1F31E;'; 
   }
 }
 
